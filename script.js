3b8c516ec596e7f140497141d812a1f91ddcd0c3
Ha Phan <ha.markus.phan@gmail.com>
2023-10-28 20:35:01 +0100

    code refactor

--- script.js
+++ script.js
@@ -88,20 +88,21 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+var characterArr = [specialCharacters,numericCharacters,lowerCasedCharacters,upperCasedCharacters];
+
 // Function to prompt user for password options
 function getPasswordOptions(arr) {
-  var f1 = confirm("Click Confirm to include special characters");
-  arr.push(f1);
-  var f2 = confirm("Click Confirm to include numeric characters");
-  arr.push(f2);
-  var f3 = confirm("Click Confirm to include lowercase characters");
-  arr.push(f3);
-  var f4 = confirm("Click Confirm to include uppercase characters");
-  arr.push(f4);
-  console.log("Confirm option for the password: Special characters: " + f1 + ", Numeric: " + f2 + ", Lowercase: " + f3 + ", and Uppercase characters: " + f4 + ".");
-  if (f1 == false && f2 == false && f3 == false && f4 == false){
-    alert("Your password options are not valid. The password needs to contain either lowercase, uppercase, numeric or special characters");
-    return;
+  var x1 = confirm("Click Confirm to include special characters");
+  arr.push(x1);
+  var x2 = confirm("Click Confirm to include numeric characters");
+  arr.push(x2);
+  var x3 = confirm("Click Confirm to include lowercase characters");
+  arr.push(x3);
+  var x4 = confirm("Click Confirm to include uppercase characters");
+  arr.push(x4);
+  console.log("Confirm option for the password: Special characters: " + x1 + ", Numeric: " + x2 + ", Lowercase: " + x3 + ", and Uppercase characters: " + x4 + ".");
+  if (x1 == false && x2 == false && x3 == false && x4 == false){
+    return alert("Your password options are not valid. The password needs to contain either lowercase, uppercase, numeric or special characters");
   } else {return arr}
 }
 
@@ -110,14 +111,14 @@ function getRandom(min,max) {
   return Math.floor(Math.random() * (max-min+1)) + min;
 }
 
-//x is the number of characters user want to input, equal to "num" value
-//y is the number of character types, equal to "quantity" value
+//x is the number of characters from user input 
+//y is the number of character types, from getPasswordOptions(arr) function
 //arr is the array to contain the number of characters for each type
 function getRandomNumCharacter(x,y,arr){
   if (y == 1){
     return [arr.push(x)];
-  };
-  var pikachu = getRandom(1,x);
+  }
+  let pikachu = getRandom(1,x);
   // console.log(pikachu);
   return [arr.push(pikachu),
           getRandomNumCharacter((x-pikachu), (y-1), arr),
@@ -140,55 +141,60 @@ function getChar(i,arr){
 // Function to generate password with user input - This is the main one.
 function generatePassword() {
   //To determine the number of characters in the password
-  var num = prompt("How many characters do you want your Password to contain?");
+  let num = prompt("How many characters do you want your Password to contain?");
   num = +num;
   console.log("The number of characters in the password: " + num);
   if (num < 8){
-    alert("The number of characters must be more than 8");
-    return;
+    return alert("The number of characters must be more than 8");
   } else if (num > 128){
-    alert("The number of characters must be less than 129");
-    return;
+    return alert("The number of characters must be less than 129");
   }
 
   //To decide which type of characters will be in the password
-  var option = [];
-  var quantity = 0;
+  let option = [];
+  let quantityType = 0;
   getPasswordOptions(option);
   option.forEach(function(i){
     if (i === true){
-      quantity++};
+      quantityType++};
   })
-  console.log("The number of types of characters in the password: " + quantity);
+  console.log("Number of character types in the password: " + quantityType);
   
   //To store random number of characters for each selected character types
-  var NumberCharacters = [];
-  getRandomNumCharacter(num,quantity,NumberCharacters);
+  let NumberCharacters = [];
+  getRandomNumCharacter(num,quantityType,NumberCharacters);
   NumberCharacters.forEach(function(i){
     if (i <=0){
       NumberCharacters =[];
-      getRandomNumCharacter(num,quantity,NumberCharacters);
+      getRandomNumCharacter(num,quantityType,NumberCharacters);
     }
   })
   console.log("The number of characters for choosen types: " + NumberCharacters);
   
   // Match the numner defined in the "NumberCharacters" to the "option"
-  var optionPassword = [];
-  option.forEach(function(i){
-    if (i == false){
+  let optionPassword = [];
+  for (let i = 0; i< option.length;i++){
+    if (option[i] == false){
       NumberCharacters.splice(i,0,0);
     }
-  })
+  }
+  // let test=[];
+  // option.map(function(i){
+  //   if (i == false){
+  //     NumberCharacters.splice(i,0,0);
+  //   }
+  // }) Not sure why this one doesn't work??
   console.log("The number of characters for all character types: " + NumberCharacters);
 
   // Randomise characters from each types using the input from NumnberCharacters
-  var randomArr = [specialCharacters,numericCharacters,lowerCasedCharacters,upperCasedCharacters];
-  console.log(randomArr);
-  var passwordGen = "";
-  console.log(passwordGen);
+  let passwordGen = "";
   for (let i = 0; i< NumberCharacters.length;i++){
-    passwordGen += getChar(NumberCharacters[i],randomArr[i]);
+    passwordGen += getChar(NumberCharacters[i],characterArr[i]);
   }
+  console.log(passwordGen);
+
+  // Shuffle your password - use Fisher Yates Method
+
   return passwordGen;
 }
 
