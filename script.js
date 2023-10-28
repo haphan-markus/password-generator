0f78c995d0f95b167f3825604b8b5ad702158e4b
Ha Phan <ha.markus.phan@gmail.com>
2023-10-28 10:31:05 +0100

    work on generatePassword

---
+++ script.js
@@ -0,0 +1,134 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+  var num = prompt("How many characters do you want your Password to contain?");
+  if (num < 8){
+    alert("The number of characters must be more than 8");
+    return;
+  } else if (num > 128){
+    alert("The number of characters must be less than 129");
+    return;
+  }
+  var specialCharactersChoice = confirm("Click Confirm to include special characters");
+  console.log(specialCharactersChoice);
+  var numericCharactersChoice = confirm("Click Confirm to include numeric characters");
+  console.log(numericCharactersChoice);
+  var lowerCasedCharactersChoice = confirm("Click Confirm to include lowercased characters");
+  console.log(lowerCasedCharactersChoice);
+  var upperCasedCharactersChoice = confirm("Click Confirm to include uppercased characters");
+  console.log(upperCasedCharactersChoice);
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
